57972c569ad361490a844e6fc7a1dd398b671b4f
TK Nguyen <tk@EverMind.local>
2016-08-10 21:09:08 +0700

    milestone 4

--- lib/movies.rb
+++ lib/movies.rb
@@ -1,9 +1,26 @@
 require "movies/version"
-
+require 'mechanize'
 module Movies
   class Search
+    def agent
+      Mechanize.new
+    end
+
+    def page
+      agent.get "http://123phim.vn/phim"   
+    end
+
+
+    def film_links
+      page.links_with(class: 'film-name')
+    end
+
     def films
-      []
+      film_links.map(&:text)  
     end
   end
+
+
+
 end
+
--- spec/movies_spec.rb
+++ spec/movies_spec.rb
@@ -4,33 +4,30 @@ describe Movies do
   it 'has a version number' do
     expect(Movies::VERSION).not_to be nil
   end
+  
+  describe "Search" do
+    let(:search) { Movies::Search.new }
 
-  it 'Search' do
-    let(:search) {Movies::Search.new}
+    describe "#films" do 
+      it "should list movies" do 
+        expect(search.films).to eq [ "Biệt Đội Cảm Tử - Suicide Squad (NC16)", "Vùng Nước Tử Thần - The Shallows (NC16)", "Siêu Điệp Viên Tái Xuất - Jason Bourne (NC16)", "Thám Tử Lừng Danh Conan: Cơn Ác Mộng Đen Tối", "Hồn Ma Trở Lại - The Remains (NC16)", "The BFG - Chuyện Chưa Kể Ở Xứ Sở Khổng Lồ", "Ác Mộng Bóng Đêm - Lights Out (NC16)", "Big Bang MADE: The Movie", "Mầm Chết - Viral (NC16)", "Biệt Đội Bắt Ma 2 - Ghostbusters 2 (NC16)", "Tik Tak, Anh Yêu Em", "Star Trek: Không Giới Hạn - Star Trek Beyond", "Tẩu Thoát Ngoạn Ngục - Skiptrace", "Fan Cuồng", "Kỷ Băng Hà: Trời Sập - Ice Age: Collision Course", "Đẳng Cấp Thú Cưng - The Secret Life of Pets", "Kỳ Nghỉ Hè của Nicolas - Nicolas on Holiday", "Cặp Đôi Trái Ngược – Pas Son Genre", "Angry Birds", "Bệnh Viện Ma (NC16)", "Mỹ Nhân Ngư - The Mermaid"] 
+      end
+    end
 
-    describe "#films" do
-			it "Should list movies" do
-				expect(search.films).to eq
-				[ "Biệt Đội Cảm Tử - Suicide Squad (NC16)",
-			"Vùng Nước Tử Thần - The Shallows (NC16)",
-			"Siêu Điệp Viên Tái Xuất - Jason Bourne (NC16)",
-			"Thám Tử Lừng Danh Conan: Cơn Ác Mộng Đen Tối",
-			"Hồn Ma Trở Lại - The Remains (NC16)",
-			"The BFG - Chuyện Chưa Kể Ở Xứ Sở Khổng Lồ",
-			"Ác Mộng Bóng Đêm - Lights Out (NC16)",
-			"Big Bang MADE: The Movie",
-			"Mầm Chết - Viral (NC16)",
-			"Biệt Đội Bắt Ma 2 - Ghostbusters 2 (NC16)",
-			"Tik Tak, Anh Yêu Em",
-			"Star Trek: Không Giới Hạn - Star Trek Beyond",
-			"Tẩu Thoát Ngoạn Ngục - Skiptrace",
-			"Fan Cuồng",
-			"Kỷ Băng Hà: Trời Sập - Ice Age: Collision Course",
-			"Đẳng Cấp Thú Cưng - The Secret Life of Pets",
-			"Angry Birds",
-			"Bệnh Viện Ma (NC16)", "Mỹ Nhân Ngư - The Mermaid"]
-			end
-		end
+    describe "#agent" do
+      it "should be an instance of Mechanize" do
+        expect(search.agent).to be_a Mechanize
+      end
+    end
+
+    describe "#page" do
+      it "should visit 'http://123phim.vn/phim'" do
+        expect(search.page).to be_a Mechanize::Page
+        expect(search.page.uri.to_s).to eq "http://www.123phim.vn/phim"
+      end
+    end
 
   end
 end
+
+
